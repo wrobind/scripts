180a8e4b6a84e51bd427dbfc191c9e77830bbfd7
Robin Webber <robindwebber@gmail.com>
2018-09-21 12:22:57 -0400

    stumbling first steps

--- vcf2csv.py
+++ vcf2csv.py
@@ -1,7 +1,24 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 
-// convert VCF contacts file to CSV format file, listing labels as first line of CSV
+# convert VCF contacts file to CSV format file, listing labels as first line of CSV
 
 import vobject
+
+# for development purposes, static file name
+
+infile = 'Nelson2018-08.vcf'
+outfile = 'Nelson2018-08.csv'
+
+with open(infile) as inf:
+	indata = inf.read()
+	vcollection = vobject.readComponents(indata)
+	vinstance = next(vcollection, None)
+	while vinstance is not None:
+		print(vinstance.contents)
+		print('  separator  ')
+		print(vinstance.contents['prodid'])
+		print('  =======================================  ')
+		#vinstance.prettyPrint()
+		vinstance = next (vcollection, None)
 
 
