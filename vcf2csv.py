ed4e0667b079e38282c3908c2d80a2ad657a5d08
Robin Webber <robindwebber@gmail.com>
2018-09-22 20:18:47 -0400

    first cut at contributions

--- vcf2csv.py
+++ vcf2csv.py
@@ -6,54 +6,79 @@ import re
 import vobject
 import csv
 
-
-states = ['IA', 'KS', 'UT', 'VA', 'NC', 'NE', 'SD', 'AL', 'ID', 'FM', 'DE', 'AK', 'CT', 'PR', 'NM', 'MS', 'PW', 'CO', 'NJ', 'FL', 'MN', 'VI', 'NV', 'AZ', 'WI', 'ND', 'PA', 'OK', 'KY', 'RI', 'NH', 'MO', 'ME', 'VT', 'GA', 'GU', 'AS', 'NY', 'CA', 'HI', 'IL', 'TN', 'MA', 'OH', 'MD', 'MI', 'WY', 'WA', 'OR', 'MH', 'SC', 'IN', 'LA', 'MP', 'DC', 'MT', 'AR', 'WV', 'TX']
-
-fn_states = re.compile(r'(' + '|'.join(states) + r')\b' + r'[ -]?(\d+|[SG])', re.IGNORECASE)
-
 # for development purposes, static file name
 
 infile = 'Nelson2018-08.vcf'
 outfile = 'Nelson2018-08.csv'
 
+# political taxonomy
+
+states = ['IA', 'KS', 'UT', 'VA', 'NC', 'NE', 'SD', 'AL', 'ID', 'FM', 'DE', 'AK', 'CT', 'PR', 'NM', 'MS', 'PW', 'CO', 'NJ', 'FL', 'MN', 'VI', 'NV', 'AZ', 'WI', 'ND', 'PA', 'OK', 'KY', 'RI', 'NH', 'MO', 'ME', 'VT', 'GA', 'GU', 'AS', 'NY', 'CA', 'HI', 'IL', 'TN', 'MA', 'OH', 'MD', 'MI', 'WY', 'WA', 'OR', 'MH', 'SC', 'IN', 'LA', 'MP', 'DC', 'MT', 'AR', 'WV', 'TX']
+
+org_federal_seat = re.compile(r'(' + '|'.join(states) + r')[ -]+(\d+|S|G|AG)+', re.IGNORECASE)
+org_state_seat = re.compile(r'(' + '|'.join(states) + r') ([hs]-\d+)', re.IGNORECASE)
+seat_safety = re.compile(r'(safe)([DR])?\b', re.IGNORECASE)
+
+note_contribution = re.compile(r'\b(\d{4}-\d{2}-\d{2}\s+#\d+\s\$[0-9,.]+\b)+')
+
+
 with open(infile) as inf:
 	contacts = vobject.readComponents(inf.read())
 	inf.close()
 
-records = []
-fieldnames = ['ST', 'DIST', 'FAMILY', 'GIVEN', 'FULLNAME', 'NOTE', 'ORG', 'ADDRESS', 'EMAIL', 'URL']
 
+fieldnames = ['ST', 'DIST', 'LEVEL', 'SAFE', 'CONTRIBS', 'FAMILY', 'GIVEN', 'FULLNAME', 'NOTE', 'DETAIL', 'ADDRESS', 'EMAIL', 'URL']
+
+records = []
 for contact in contacts:
-	record = {'ST': '  ', 'DIST': '   ', 'FULLNAME': '       ' }
-	if 'fn' in contact.contents:
-		if 'x-abshowas' in contact.contents:
-			# if "show as" company, not the candidate
-			if contact.contents['x-abshowas'] == 'COMPANY':
-				record['ORG'] = contact.fn.value
-			else:
-				record['FULLNAME'] = contact.fn.value
-		# "FN" (Formattted Name) field in Helm's contact list is often state-district
-		m = fn_states.match(contact.fn.value)
+	if 'fn' not in contact.contents:
+		continue
+
+	record = {}
+	record['LEVEL'] = 'UNKN'
+
+	if 'org' in contact.contents:
+		m = org_federal_seat.match(contact.org.value[0])
 		if m is not None:
-			record['ST'] = m.group(1)
-			record['DIST'] = m.group(2)
+			record['LEVEL'] = 'FEDERAL'
+			record['ST'] = m.group(1).upper()
+			record['DIST'] = m.group(2).upper()
+		else:
+			m = org_state_seat.match(contact.org.value[0])
+			if m is not None:
+				record['LEVEL'] = 'STATE'
+				record['ST'] = m.group(1).upper()
+				record['DIST'] = m.group(2).upper()
+		m = seat_safety.search(contact.org.value[0])
+		if m is not None:
+			if m.group(2) is not None:
+				record['SAFE'] = m.group(2).upper()
+			else:
+				record['SAFE'] = 'U'
+		record['DETAIL'] = ';'.join(contact.org.value)
+
+	if 'note' in contact.contents:
+		if 'SAFE' not in record or record['SAFE'] == 'U':
+			m = seat_safety.search(contact.note.value)
+			if m is not None:
+				if m.group(2) is not None:
+					record['SAFE'] = m.group(2).upper()
+				else:
+					record['SAFE'] = 'U'
+		m = note_contribution.findall(contact.note.value)
+		if m is not None:
+			record['CONTRIBS'] = '@'.join(m)
+		record['NOTE'] = contact.note.value
+
 
 	if 'n' in contact.contents:
 		record['GIVEN'] = contact.n.value.given
 		record['FAMILY'] = contact.n.value.family
-		if 'fn' not in contact.contents:
-			record['FULLNAME'] = contact.n.value
-			
-
-
+		record['FULLNAME'] = contact.n.value
 	if 'adr' in contact.contents:
 		record['ADDRESS'] = contact.adr.value
 	# if 'email' in contact.contents:
 		# record['EMAIL'] = contact.email.value
-	if 'note' in contact.contents:
-		record['NOTE'] = contact.note.value
-	if 'org' in contact.contents:
-		record['ORG'] = contact.org.value
 	if 'url' in contact.contents:
 		record['URL'] = contact.url.value
 
